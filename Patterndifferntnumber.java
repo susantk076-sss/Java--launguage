04e4680b823a1cd90ee7fd6e6ca91dc55c0d7fb3
Hariom Sharma <susantk076@gmail.com>
2026-09-13 00:36:20 +0530

    Add files via upload

---
+++ Patterndifferntnumber.java
@@ -0,0 +1,15 @@
+public class Patterndifferntnumber {
+    public static void main(String[] args) {
+        int num = 1;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print( " ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
